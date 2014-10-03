429453b2e9c18950b0534f82c300443a60566390
armyinf0703 <armyinf0703@hotmail.com>
2014-10-03 15:03:06 -0400

    Recruiting server mod pack
    yeah

---
+++ Recruiting Server MOD PACK/mod.cpp
@@ -0,0 +1,5 @@
+   name = "15th MEU Recruiting Pack";
+   picture = "meu_logo.paa"; //optional to use different mod picture, mod.paa is default
+   actionName = "Website";
+   action = "http://www.15thmeu.net"; //only websites are currently possible here
+   
