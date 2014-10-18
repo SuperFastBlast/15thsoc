b5edfba5741e90773c2c76c8231de84a53bda1b1
armyinf0703 <armyinf0703@hotmail.com>
2014-10-18 02:24:33 -0400

    muzzlevelocity pbo

---
+++ 15th_weapons/Muzzlevelocity_v1.01/userconfig/fnc_muzzlevelocity/fnc_muzzlevelocity_weapons.hpp
@@ -0,0 +1,10 @@
+//Add non-stock weapons here. These entries will be included in config.cpp
+/*
+class MyWeapon: M16A2
+{
+	muzzleVelocityFactor = 0.946; 	// (muzzlevelocity of my weapon)/(muzzle velocity of "baseweapon" for the calibre) = muzzleVelocityFactor
+									// check config.cpp for the baseweapons
+};
+*/
+
+
