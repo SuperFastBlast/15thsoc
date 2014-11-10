c5a85a76b4b7d2950dafaed48f3d6376e32e75af
sw4l <shhs11@live.com>
2014-11-09 20:50:53 -0600

    ingame, physx work in progress. seperated into its own HPP file for easy debug and editing. needs damper animations worked on, new mk19 defined or use one from a3 (idk if it exists)

--- 15th_vehicles/GMVS/config.cpp
+++ 15th_vehicles/GMVS/config.cpp
@@ -1,3 +1,50 @@
+/*
+Key words for some of my short hand on notes
+Orig. = original
+----------------------------------------------------
+List of magazines & weapons to add to the inventory :
+Basic supplys, stanags, grenates and med supplies.
+----------------------------------------------------
+check on adding Ctab and LR radios to the SOV
+*/
+////////////stiff under this goes in cfgvehicles
+				//Picture = "\CA\wheeled_e\Data\UI\Picture_hmmwv_sov_CA.paa"; grab these later
+		//Icon = "\CA\wheeled_e\Data\UI\Icon_hmmwv_sov_CA.paa";
+		//driverAction = "HMMWV_Driver_EP1";
+		//cargoAction[] = {"HMMWV_Cargo01_EP1","HMMWV_Cargo02_EP1","HMMWV_Cargo03_EP1","HMMWV_Cargo04_EP1"};
+		/*
+		put sounds in, around this area
+		*/
+		//crew = "US_Delta_Force_EP1"; //Recon guys?
+		//typicalCargo[] = {"US_Delta_Force_EP1","US_Delta_Force_EP1","US_Delta_Force_EP1"};//reconguys?
+		/* FIND OUT WHAT RECON WANTS IN THE BASE LOADOUT OF THE VEHICLE
+		class TransportMagazines
+		{
+			class _xx_30Rnd_556x45_Stanag
+			{
+				magazine = "30Rnd_556x45_Stanag";
+				count = 15;
+			};
+			class _xx_HandGrenade_West
+			{
+				magazine = "HandGrenade_West";
+				count = 2;
+			};
+			class _xx_SmokeShellGreen
+			{
+				magazine = "SmokeShellGreen";
+				count = 2;
+			};
+			class _xx_SmokeShell
+			{
+				magazine = "SmokeShell";
+				count = 2;
+			};
+		};
+		class TransportWeapons{};
+		*/
+		////end the CfgVeh add
+
 class CfgPatches
 {
 	class meu_recon_vehicles
@@ -42,7 +89,8 @@ class CfgVehicles {
 			class HitGlass4;
 		};
 	};
-	class HMMWV_base: Car_f
+	class MRAP_02_base_F : Car_f{};
+	class HMMWV_base: MRAP_02_base_F
 	{
 		htMin = 60;
 		htMax = 1800;
@@ -110,43 +158,40 @@ class CfgVehicles {
 				armor = 1;
 			};
 		};
-	};
+	};//end base
 
-		class meu_GMVS {
+		class meu_GMVS : HMMWV_base {
 		scope = 2;
 		transportSoldier = 4;
 		vehicleClass = "Car";
 		transportMaxBackpacks = 7;
 		model = "GMVS\M998A2_sov";
 		displayname = "GMVS";
-		simulation = "Car";
 		armor = 40;
 		damageResistance = 0.00581;
 		threat[] = {1,0.3,0.3};
 		side = TWest;
+		selectionfireanim = "zasleh";
 		faction = BLU_F;
-		//Picture = "\CA\wheeled_e\Data\UI\Picture_hmmwv_sov_CA.paa"; grab these later
-		//Icon = "\CA\wheeled_e\Data\UI\Icon_hmmwv_sov_CA.paa";
-		//driverAction = "HMMWV_Driver_EP1";
-		//cargoAction[] = {"HMMWV_Cargo01_EP1","HMMWV_Cargo02_EP1","HMMWV_Cargo03_EP1","HMMWV_Cargo04_EP1"};
-		/*
-		put sounds in, around this area
-		*/
+		selectionClan = "";
 
+		#include "physx.hpp" // seperating files to keep the config clean
 		class Turrets: Turrets
 		{
 			class MainTurret: MainTurret
 			{
 				weapons[] = {"MK19"};
-				magazines[] = {"48Rnd_40mm_MK19","48Rnd_40mm_MK19","48Rnd_40mm_MK19","48Rnd_40mm_MK19"};
-				soundServo[] = {"\Ca\sounds\Vehicles\Servos\turret-1",0.01,1.0,10};
-				gunnerAction = "HMMWV_Gunner03_EP1";
+				magazines[] = {"48Rnd_40mm_MK19","48Rnd_40mm_MK19","48Rnd_40mm_MK19","48Rnd_40mm_MK19"}; // prob need to check weapon defs on this
+				soundServo[] = {"",0.01,1.0,10};//\Ca\sounds\Vehicles\Servos\turret-1 orgi. 
+				gunnerAction = ""; //HMMWV_Gunner03_EP1 orig.
+				selectionfireanim = "zasleh";
 				class GunFire: WeaponCloudsMGun
 				{
 					interval = 0.01;
 				};
 				commanding = 0;
 				primaryGunner = 0;
+				outgunnermayfire = 1;
 				primaryObserver = 0;
 				LODTurnedOut = 1000;
 				LODTurnedIn = 1000;
@@ -154,9 +199,9 @@ class CfgVehicles {
 			class SideTurret: MainTurret
 			{
 				gunnerName = "$STR_POSITION_FRONTGUNNER";
-				weapons[] = {"M240_veh"};
-				magazines[] = {"100Rnd_762x51_M240","100Rnd_762x51_M240","100Rnd_762x51_M240","100Rnd_762x51_M240"};
-				soundServo[] = {"\ca\wheeled\Data\Sound\servo3",0.0001,1.1};
+				weapons[] = {"M240_veh"};// check definitions for this
+				magazines[] = {"100Rnd_762x51_M240","100Rnd_762x51_M240","100Rnd_762x51_M240","100Rnd_762x51_M240"}; // check definitions for this
+				soundServo[] = {"",0.0001,1.1}; // \ca\wheeled\Data\Sound\servo3 orgi.
 				gunnerAction = "HMMWV_Gunner02_EP1";
 				commanding = 1;
 				primaryGunner = 1;
@@ -195,34 +240,7 @@ class CfgVehicles {
 				weapon = "M240_veh";
 			};
 		};
-		//crew = "US_Delta_Force_EP1"; //Recon guys?
-		//typicalCargo[] = {"US_Delta_Force_EP1","US_Delta_Force_EP1","US_Delta_Force_EP1"};//reconguys?
-		/* FIND OUT WHAT RECON WANTS IN THE BASE LOADOUT OF THE VEHICLE
-		class TransportMagazines
-		{
-			class _xx_30Rnd_556x45_Stanag
-			{
-				magazine = "30Rnd_556x45_Stanag";
-				count = 15;
-			};
-			class _xx_HandGrenade_West
-			{
-				magazine = "HandGrenade_West";
-				count = 2;
-			};
-			class _xx_SmokeShellGreen
-			{
-				magazine = "SmokeShellGreen";
-				count = 2;
-			};
-			class _xx_SmokeShell
-			{
-				magazine = "SmokeShell";
-				count = 2;
-			};
-		};
-		class TransportWeapons{};
-		*/
+
 		class Damage
 		{
 			tex[] = {};
