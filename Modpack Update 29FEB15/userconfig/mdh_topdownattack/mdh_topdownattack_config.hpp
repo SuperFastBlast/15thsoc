aef19717a972cffc1a2fce69b8fa4ecbaae4f1f1
Karneck <dschultz26@hotmail.com>
2015-02-24 11:33:17 -0800

    av8b update

--- Modpack Update 29FEB15/userconfig/mdh_topdownattack/mdh_topdownattack_config.hpp
+++
@@ -1,9 +0,0 @@
-// Userconfig for mdh_topdownattack mod
-// You can change your key combination here for the firemode switch (TopDown/Direct)
-// All available keys can be found at https://community.bistudio.com/wiki/ListOfKeyCodes
-// Default firemode switch key is 15 = Tabulator
-#define MDH_TOPDOWNATTACK_KEY 15
-// so if you want to use the "TAB + shift" shortcut then just set MDH_TOWDOWNATTACK_SHIFT to true
-#define MDH_TOPDOWNATTACK_SHIFT false
-#define MDH_TOPDOWNATTACK_CTRL false
-#define MDH_TOPDOWNATTACK_ALT false
