4caac2eabdee70206ca8e33e77a534b57aeac8b8
MrPiipe <piipe.donado@hotmail.com>
2015-03-12 11:59:02 -0500

    Config additions
    Turrets working - Need to fix animations
    Added FFV - Almost done, small tweaks and ability to get into FFV
    positions from outside.
    Added LR radios

--- 15th_vehicles/GMVS/config.cpp
+++ 15th_vehicles/GMVS/config.cpp
@@ -5,12 +5,12 @@ Orig. = original
 
 
 List to add:
-Work on turrets
-FFV
+Work on turrets - Fix animations
+FFV - Almost done, small tweaks and ability to get into FFV positions from outside.
 Add Physx
 Add cTab compatibility
-Add LR radios
-Add inventory
+Add LR radios - DONE
+Add inventory - Waiting for FORECON's list
 
 Add crewed version
 Add sounds
@@ -102,6 +102,25 @@ class CfgVehicles {
 		};
 	};
 	class MRAP_02_base_F : Car_f{};
+		class SpeechVariants {
+			class Default {
+				speechSingular[] = {"veh_vehicle_armedcar_s"};
+				speechPlural[] = {"veh_vehicle_armedcar_p"};
+			};
+		};
+		
+		tf_RadioType_api = "tf_rt1523g";
+		tf_hasLRradio_api = 1;
+		tf_isolatedAmount_api = 0.5;
+		
+		transportMaxBackpacks = 6;
+		
+		maxSpeed = 100;	// max speed on level road, km/h
+		acceleration = 40;
+		textSingular = "$STR_A3_nameSound_veh_vehicle_armedcar_s";
+		textPlural = "$STR_A3_nameSound_veh_vehicle_armedcar_p";
+		nameSound = "veh_vehicle_armedcar_s";
+		
 	class HMMWV_base: MRAP_02_base_F
 	{
 		htMin = 60;
@@ -110,7 +129,6 @@ class CfgVehicles {
 		mfMax = 100;
 		mFact = 1;
 		tBody = 200;
-		transportMaxBackpacks = 5;
 		class Turrets: Turrets
 		{
 			class MainTurret: MainTurret
@@ -123,23 +141,23 @@ class CfgVehicles {
 		{
 			class HitLFWheel: HitLFWheel
 			{
-				armor = 0.15;
+				armor = 0.25;
 			};
 			class HitLBWheel: HitLBWheel
 			{
-				armor = 0.15;
+				armor = 0.25;
 			};
 			class HitRFWheel: HitRFWheel
 			{
-				armor = 0.15;
+				armor = 0.25;
 			};
 			class HitRBWheel: HitRBWheel
 			{
-				armor = 0.15;
+				armor = 0.25;
 			};
 			class HitFuel
 			{
-				armor = 0.14;
+				armor = 0.24;
 				material = -1;
 				name = "fueltank";
 				visual = "";
@@ -147,7 +165,7 @@ class CfgVehicles {
 			};
 			class HitEngine
 			{
-				armor = 0.5;
+				armor = 0.75;
 				material = -1;
 				name = "engine";
 				visual = "";
@@ -170,6 +188,53 @@ class CfgVehicles {
 				armor = 1;
 			};
 		};
+		class Reflectors {
+			class LightCarHeadL01 {
+				color[] = {1900, 1800, 1700};
+				ambient[] = {5, 5, 5};
+				position = "LightCarHeadL01";
+				direction = "LightCarHeadL01_end";
+				hitpoint = "Light_L";
+				selection = "Light_L";
+				size = 1;
+				innerAngle = 100;
+				outerAngle = 179;
+				coneFadeCoef = 10;
+				intensity = 1;
+				useFlare = 1;
+				dayLight = 0;
+				flareSize = 1;
+				
+				class Attenuation {
+					start = 1;
+					constant = 0;
+					linear = 0;
+					quadratic = 0.25;
+					hardLimitStart = 30;
+					hardLimitEnd = 60;
+				};
+			};
+			
+			class LightCarHeadL02 : LightCarHeadL01 {
+				position = "LightCarHeadL02";
+				direction = "LightCarHeadL02_end";
+				FlareSize = 0.5;
+			};
+			
+			class LightCarHeadR01 : LightCarHeadL01 {
+				position = "LightCarHeadR01";
+				direction = "LightCarHeadR01_end";
+				hitpoint = "Light_R";
+				selection = "Light_R";
+			};
+			
+			class LightCarHeadR02 : LightCarHeadR01 {
+				position = "LightCarHeadR02";
+				direction = "LightCarHeadR02_end";
+				FlareSize = 0.5;
+			};
+		};
+		aggregateReflectors[] = {{"Left", "Right", "Left2", "Right2"}};
 	};//end base
 
 		class meu_GMVS : HMMWV_base {
@@ -186,21 +251,31 @@ class CfgVehicles {
 		selectionfireanim = "zasleh";
 		faction = BLU_F;
 		selectionClan = "";
+		accuracy = 0.3;
+		
 
+		class CargoTurret;
 		#include "physx.hpp" // seperating files to keep the config clean
 		class Turrets: Turrets
 		{
 			class MainTurret: MainTurret
 			{
 				weapons[] = {"GMG_40mm"};
-				magazines[] = {"200Rnd_40mm_G_belt", "96Rnd_40mm_G_belt", "64Rnd_40mm_G_belt", "32Rnd_40mm_G_belt"};//magazines[] = {"48Rnd_40mm_MK19","48Rnd_40mm_MK19","48Rnd_40mm_MK19","48Rnd_40mm_MK19"}; // prob need to check weapon defs on this
-				soundServo[] = {"",0.01,1.0,10};//\Ca\sounds\Vehicles\Servos\turret-1 orgi. 
-				gunnerAction = ""; //HMMWV_Gunner03_EP1 orig.
+				magazines[] = {"48Rnd_40mm_MK19","48Rnd_40mm_MK19","48Rnd_40mm_MK19","48Rnd_40mm_MK19"}; //magazines[] = {"200Rnd_40mm_G_belt", "96Rnd_40mm_G_belt", "64Rnd_40mm_G_belt", "32Rnd_40mm_G_belt"}; // prob need to check weapon defs on this
+				soundServo[] = {"A3\sounds_f\dummysound", db-40, 1.0, 10}; //soundServo[] = {"",0.01,1.0,10};   \Ca\sounds\Vehicles\Servos\turret-1 orig. 
+				gunnerAction = "HMMWV_Gunner03_EP1"; // orig.
+				discreteDistance[] = {100,200,300,400,500,600,800,1000,1200,1500};
+				discreteDistanceInitIndex = 2;
 				selectionfireanim = "zasleh";
+				minElev = -25;
+				maxElev = 60;
 				class GunFire: WeaponCloudsMGun
 				{
 					interval = 0.01;
 				};
+				viewGunnerInExternal = "true";
+				castGunnerShadow = true;
+				stabilizedInAxes = "StabilizedInAxesNone";
 				commanding = 0;
 				primaryGunner = 0;
 				body = "MainTurret";
@@ -239,6 +314,54 @@ class CfgVehicles {
 				animationSourceGun = "SideGun";
 				LODTurnedOut = 1200;
 				LODTurnedIn = 1200;
+			};
+			
+			class CargoTurret_01: CargoTurret
+			{
+				gunnerAction = "passenger_inside_3";
+				gunnerCompartments = "Compartment1";
+				memoryPointsGetInGunner = "pos Cargo1";
+				memoryPointsGetInGunnerDir = "pos Cargo dir1";
+				gunnerName = "Cargo Gunner FL";
+				proxyIndex = 1;
+				maxElev = 45;
+				minElev = -35;
+				maxTurn = 115;
+				minTurn = 20;
+				isPersonTurret = 1;
+				ejectDeadGunner = 0;
+				memoryPointGunnerOptics = "eye";
+				class dynamicViewLimits{};
+			};
+			class CargoTurret_02: CargoTurret_01
+			{
+				memoryPointsGetInGunner = "pos Cargo2";
+				memoryPointsGetInGunnerDir = "pos Cargo dir2";
+				gunnerName = "Cargo Gunner FR";
+				proxyIndex = 2;
+				maxTurn = -20;
+				minTurn = -115;
+				class dynamicViewLimits{};
+			};
+			class CargoTurret_03: CargoTurret_01
+			{
+				memoryPointsGetInGunner = "pos Cargo3";
+				memoryPointsGetInGunnerDir = "pos Cargo dir3";
+				gunnerName = "Cargo Gunner RL";
+				proxyIndex = 3;
+				maxTurn = -15;
+				minTurn = -115;
+				class dynamicViewLimits{};
+			};
+			class CargoTurret_04: CargoTurret_03
+			{
+				memoryPointsGetInGunner = "pos Cargo4";
+				memoryPointsGetInGunnerDir = "pos Cargo dir4";
+				gunnerName = "Cargo Gunner RR";
+				proxyIndex = 4;
+				maxTurn = 115;
+				minTurn = 15;
+				class dynamicViewLimits{};
 			};
 		};
 		class AnimationSources: AnimationSources
@@ -405,31 +528,7 @@ class CfgAmmo
 		hit = 12;
 	};
 	class G_40mm_HE;
-	class 200Rnd_40mm_G_belt: G_40mm_HE
-	{
-		caliber = 1.5;
-		hit = 8;
-		indirecthit = 10;
-		indirecthitrange = 5.5;
-		whistledist = 5.5;
-	};
-	class 96Rnd_40mm_G_belt: G_40mm_HE
-	{
-		caliber = 1.5;
-		hit = 8;
-		indirecthit = 10;
-		indirecthitrange = 5.5;
-		whistledist = 5.5;
-	};
-	class 64Rnd_40mm_G_belt: G_40mm_HE
-	{
-		caliber = 1.5;
-		hit = 8;
-		indirecthit = 10;
-		indirecthitrange = 5.5;
-		whistledist = 5.5;
-	};
-	class 32Rnd_40mm_G_belt: G_40mm_HE
+	class 48Rnd_40mm_MK19: G_40mm_HE
 	{
 		caliber = 1.5;
 		hit = 8;
@@ -452,42 +551,12 @@ class cfgMagazines
 		displayname = "100rnd 7.62mm Box";
 		mass = 30;
 	};
-	class 200Rnd_40mm_G_belt: VehicleMagazine
+	class 48Rnd_40mm_MK19: VehicleMagazine
 	{
 		scope = 2;
 		displayName = "High Explosive";
-		ammo = "200Rnd_40mm_G_belt";
-		count = 200;
-		initSpeed = 185;
-		maxLeadSpeed = 100;
-		nameSound = "grenadelauncher";
-	};
-	class 96Rnd_40mm_G_belt: VehicleMagazine
-	{
-		scope = 2;
-		displayName = "High Explosive";
-		ammo = "200Rnd_40mm_G_belt";
-		count = 96;
-		initSpeed = 185;
-		maxLeadSpeed = 100;
-		nameSound = "grenadelauncher";
-	};
-	class 64Rnd_40mm_G_belt: VehicleMagazine
-	{
-		scope = 2;
-		displayName = "High Explosive";
-		ammo = "200Rnd_40mm_G_belt";
-		count = 64;
-		initSpeed = 185;
-		maxLeadSpeed = 100;
-		nameSound = "grenadelauncher";
-	};
-	class 32Rnd_40mm_G_belt: VehicleMagazine
-	{
-		scope = 2;
-		displayName = "High Explosive";
-		ammo = "200Rnd_40mm_G_belt";
-		count = 32;
+		ammo = "48Rnd_40mm_MK19";
+		count = 48;
 		initSpeed = 185;
 		maxLeadSpeed = 100;
 		nameSound = "grenadelauncher";
